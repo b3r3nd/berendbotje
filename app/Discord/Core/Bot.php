455e28021b51bb3fe7b2a25dd2686005c13e5f15
Berend de Groot <b3r3ndd@gmail.com>
2022-10-18 19:59:06 +0200

    First basic way to play music from youtube. It downloads the audio first and removes it after the audio finished playing.

--- app/Discord/Core/Bot.php
+++ app/Discord/Core/Bot.php
@@ -12,6 +12,7 @@ use App\Discord\Cringe\AddCringe;
 use App\Discord\Cringe\CringeIndex;
 use App\Discord\Cringe\DelCringe;
 use App\Discord\Music\PlayLocalFile;
+use App\Discord\Music\PlayYoutube;
 use App\Discord\Say;
 use App\Discord\SimpleCommand\AddCommand;
 use App\Discord\SimpleCommand\CommandIndex;
@@ -123,6 +124,7 @@ class Bot
         (new Say())->register();
 
         (new PlayLocalFile())->register();
+        (new PlayYoutube())->register();
     }
 
     /**
