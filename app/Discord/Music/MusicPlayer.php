f583eef51ef5fba7df6d049f93df779156e363e0
Berend de Groot <b3r3ndd@gmail.com>
2022-10-21 16:19:11 +0200

    Added actual queue, when using play command music will start playing all songs in the queue until there are none left. Music is added with addsong and will be added at the end of the queue while it is playing!

--- app/Discord/Music/MusicPlayer.php
+++ app/Discord/Music/MusicPlayer.php
@@ -8,7 +8,6 @@ use App\Models\Song;
 use Discord\Parts\Channel\Channel;
 use Discord\Parts\Channel\Message;
 use Discord\Voice\VoiceClient;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 
 class MusicPlayer
@@ -17,12 +16,14 @@ class MusicPlayer
     private static ?MusicPlayer $musicPlayer;
     private int $channelId;
 
-
     private function __construct()
     {
         $this->playing = false;
     }
 
+    /**
+     * @return MusicPlayer
+     */
     public static function getPlayer(): MusicPlayer
     {
         if (!isset(self::$musicPlayer)) {
@@ -31,66 +32,103 @@ class MusicPlayer
         return self::$musicPlayer;
     }
 
-    public function getChannel()
-    {
-        return $this->channelId;
-    }
-
-    public function setChannel(int $channelId): void
-    {
-        $this->channelId = $channelId;
-    }
-
-    public static function isPlaying(): bool
-    {
-        return self::getPlayer()->playing;
-    }
-
-    public function start(Message $message)
-    {
-        foreach ($message->channel->guild->voice_states as $voiceState) {
-            if ($voiceState->user_id === $message->author->id) {
-                $channel = Bot::getDiscord()->getChannel($voiceState->channel_id);
-
-                $song = Song::orderBy('created_at')->first();
-
-                $message->channel->sendMessage("Playing song ID {$song->id} with filename {$song->filename}");
-
-
-                Bot::getDiscord()->joinVoiceChannel($channel)->done(function (VoiceClient $voice) use ($song, $message) {
-                    $message->channel->sendMessage("Joined voice call, playing audio");
-                    $voice->playFile(Storage::path($song->filename))->then(function () use ($song, $voice, $message) {
-                        $message->channel->sendMessage("Finished playing audio, leaving voice call");
-                        $voice->close();
-                        $message->channel->sendMessage("Deleting audio from disk");
-                        Storage::delete($song->filename);
-                        $song->delete();
-                    });
-                });
-            }
-        }
-
-
-    }
-
+    /**
+     * @param string $song
+     * @return $this
+     */
     public function play(string $song): self
     {
         ProcessYoutubeDownload::dispatch($song, MusicPlayer::getPlayer());
         return $this;
     }
 
+    /**
+     * @param Message $message
+     * @return void
+     */
+    public function start(Message $message): void
+    {
+        foreach ($message->channel->guild->voice_states as $voiceState) {
+            if ($voiceState->user_id === $message->author->id) {
+                $channel = Bot::getDiscord()->getChannel($voiceState->channel_id);
+                Bot::getDiscord()->joinVoiceChannel($channel)->done(function (VoiceClient $voice) use ($message) {
+                    $message->channel->sendMessage("Joined voice call, playing audio");
+                    $song = Song::orderBy('created_at')->first();
+                    $this->playFile($song, $voice, $message);
+                });
+            }
+        }
+    }
+
+    /**
+     * @param $song
+     * @param $voice
+     * @param $message
+     * @return void
+     */
+    private function playFile($song, $voice, $message): void
+    {
+        $voice->playFile(Storage::path($song->filename))->then(function () use ($song, $voice, $message) {
+            $message->channel->sendMessage("Finished playing audio, leaving voice call");
+            $message->channel->sendMessage("Deleting audio from disk");
+            Storage::delete($song->filename);
+            $song->delete();
+            $message->channel->sendMessage("Playing next song");
+            $song = Song::orderBy('created_at')->first();
+            if ($song) {
+                $this->playFile($song, $voice, $message);
+            } else {
+                $voice->close();
+            }
+        });
+    }
+
+    /**
+     * @return int
+     */
+    public function getChannel(): int
+    {
+        return $this->channelId;
+    }
+
+    /**
+     * @param int $channelId
+     * @return void
+     */
+    public function setChannel(int $channelId): void
+    {
+        $this->channelId = $channelId;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isPlaying(): bool
+    {
+        return self::getPlayer()->playing;
+    }
+
+    /**
+     * @return $this
+     */
     public function resume(): self
     {
         $this->playing = true;
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     public function pause(): self
     {
         $this->playing = false;
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     public function stop(): self
     {
         $this->playing = false;
@@ -98,6 +136,9 @@ class MusicPlayer
     }
 
 
+    /**
+     * @return string
+     */
     public function getStatus(): string
     {
         if (!$this->playing) {
