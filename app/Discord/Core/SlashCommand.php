030a1f66578cb4d3bae336e2f8c63ccb5826aeb2
Berend de Groot <b3r3ndd@gmail.com>
2022-10-29 06:33:36 +0200

    Forgot to remove comment around dev statement, is not used on prod anyways.. so

--- app/Discord/Core/SlashCommand.php
+++ app/Discord/Core/SlashCommand.php
@@ -90,9 +90,9 @@ abstract class SlashCommand
 
         // Why you ask? I do not want to register slash commands everytime on my test bot. It takes time, it's
         // Annoying and I don't need them!
-        //  if (env('APP_ENV') != 'local') {
-        Bot::getDiscord()->application->commands->save($command);
-        // }
+        if (env('APP_ENV') != 'local') {
+            Bot::getDiscord()->application->commands->save($command);
+        }
     }
 
     public function registerMessageCommand(): void
