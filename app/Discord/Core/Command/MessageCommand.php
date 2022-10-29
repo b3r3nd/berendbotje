0a4006834ab104f6516eea9bd422204ae48e276f
Berend de Groot <b3r3ndd@gmail.com>
2022-10-29 12:03:21 +0200

    TMP fix for message arguments becuase I broke it in previous update

--- app/Discord/Core/Command/MessageCommand.php
+++ app/Discord/Core/Command/MessageCommand.php
@@ -3,6 +3,8 @@
 namespace App\Discord\Core\Command;
 
 use App\Discord\Core\Bot;
+use App\Discord\Core\EmbedFactory;
+use App\Models\DiscordUser;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Event;
@@ -34,7 +36,31 @@ abstract class MessageCommand extends Command
                 return;
             }
             if (str_starts_with($message->content, Bot::get()->getPrefix() . $this->trigger)) {
-                $this->validateMessage($message);
+                if (!DiscordUser::hasLevel($message->author->id, $this->accessLevel->value)) {
+                    $message->channel->sendMessage(EmbedFactory::failedEmbed(__("bot.lack-access")));
+                    return;
+                }
+                if ($this->requiresMention && $message->mentions->count() == 0) {
+                    $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-mention')));
+                    return;
+                }
+                if ($this->requiredArguments > 0) {
+                    $parameters = explode(' ', $message->content);
+                    if (!isset($parameters[$this->requiredArguments])) {
+                        $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-arguments', ['count' => $this->requiredArguments])));
+                        return;
+                    } else {
+                        array_shift($parameters);
+                        $this->messageString = join(' ', $this->arguments);
+                        foreach ($parameters as $index => $parameter) {
+                            if ($message->mentions->first() && str_contains($parameter, $message->mentions->first())) {
+                                $parameters[$index] = $message->mentions->first()->id;
+                            }
+                        }
+                        $this->arguments = $parameters;
+                    }
+                }
+
                 $this->message = $message;
                 $this->action();
             }
--- app/Discord/Core/Command/MessageCommandTrait.php
+++ app/Discord/Core/Command/MessageCommandTrait.php
@@ -22,32 +22,31 @@ trait MessageCommandTrait
      * @param $message
      * @return void
      */
-    protected function validateMessage($message): void
-    {
-        if (!DiscordUser::hasLevel($message->author->id, $this->accessLevel->value)) {
-            $message->channel->sendMessage(EmbedFactory::failedEmbed(__("bot.lack-access")));
-            return;
-        }
-        if ($this->requiresMention && $message->mentions->count() == 0) {
-            $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-mention')));
-            return;
-        }
-        if ($this->requiredArguments > 0) {
-            $parameters = explode(' ', $message->content);
-            if (!isset($parameters[$this->requiredArguments])) {
-                $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-arguments', ['count' => $this->requiredArguments])));
-                return;
-            } else {
-                array_shift($parameters);
-                $this->messageString = join(' ', $this->arguments);
-                foreach ($parameters as $index => $parameter) {
-                    if ($message->mentions->first() && str_contains($parameter, $message->mentions->first())) {
-                        $parameters[$index] = $message->mentions->first()->id;
-                    }
-                }
-                $this->arguments = $parameters;
-            }
-        }
-
-    }
+//    protected function validateMessage($message): void
+//    {
+//        if (!DiscordUser::hasLevel($message->author->id, $this->accessLevel->value)) {
+//            $message->channel->sendMessage(EmbedFactory::failedEmbed(__("bot.lack-access")));
+//            return;
+//        }
+//        if ($this->requiresMention && $message->mentions->count() == 0) {
+//            $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-mention')));
+//            return;
+//        }
+//        if ($this->requiredArguments > 0) {
+//            $parameters = explode(' ', $message->content);
+//            if (!isset($parameters[$this->requiredArguments])) {
+//                $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-arguments', ['count' => $this->requiredArguments])));
+//                return;
+//            } else {
+//                array_shift($parameters);
+//                $this->messageString = join(' ', $this->arguments);
+//                foreach ($parameters as $index => $parameter) {
+//                    if ($message->mentions->first() && str_contains($parameter, $message->mentions->first())) {
+//                        $parameters[$index] = $message->mentions->first()->id;
+//                    }
+//                }
+//                $this->arguments = $parameters;
+//            }
+//        }
+//    }
 }
