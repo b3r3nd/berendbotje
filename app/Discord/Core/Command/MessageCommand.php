a1af961ee644ed7ab3020fc47ae787e2f27d5735
Berend de Groot <b3r3ndd@gmail.com>
2022-10-31 20:43:24 +0100

    Finally fixed the duplicate code issue, moved code to the trait which is used in both MessageCommand and MessageAndSlashCommand

--- app/Discord/Core/Command/MessageCommand.php
+++ app/Discord/Core/Command/MessageCommand.php
@@ -3,8 +3,6 @@
 namespace App\Discord\Core\Command;
 
 use App\Discord\Core\Bot;
-use App\Discord\Core\EmbedFactory;
-use App\Models\DiscordUser;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Event;
@@ -25,46 +23,18 @@ abstract class MessageCommand extends Command
      * (message only command, or both slash and message command) something else needs to be done.
      *
      * @return void
-     * @TODO Fix duplicate code with block below
      */
     public function registerMessageCommand(): void
     {
         Bot::get()->discord()->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-            if ($message->author->bot) {
+            if (!$this->validateMessage($message, $discord)) {
                 return;
             }
-            if (str_contains($message->content, $discord->user->id)) {
-                return;
-            }
-            if (str_starts_with($message->content, Bot::get()->getPrefix() . $this->trigger)) {
-                if (!DiscordUser::hasLevel($message->author->id, $this->accessLevel->value)) {
-                    $message->channel->sendMessage(EmbedFactory::failedEmbed(__("bot.lack-access")));
-                    return;
-                }
-                if ($this->requiresMention && $message->mentions->count() == 0) {
-                    $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-mention')));
-                    return;
-                }
-                if ($this->requiredArguments > 0) {
-                    $parameters = explode(' ', $message->content);
-                    if (!isset($parameters[$this->requiredArguments])) {
-                        $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-arguments', ['count' => $this->requiredArguments])));
-                        return;
-                    } else {
-                        array_shift($parameters);
-                        foreach ($parameters as $index => $parameter) {
-                            if ($message->mentions->first() && str_contains($parameter, $message->mentions->first())) {
-                                $parameters[$index] = $message->mentions->first()->id;
-                            }
-                        }
-                        $this->arguments = $parameters;
-                        $this->messageString = join(' ', $this->arguments);
-                    }
-                }
-
-                $this->message = $message;
-                $this->action();
-            }
+            $parameters = $this->processParameters($message);
+            $this->arguments = $parameters;
+            $this->messageString = join(' ', $this->arguments);
+            $this->message = $message;
+            $this->action();
         });
     }
 }
--- app/Discord/Core/Command/SlashAndMessageCommand.php
+++ app/Discord/Core/Command/SlashAndMessageCommand.php
@@ -4,7 +4,6 @@ namespace App\Discord\Core\Command;
 
 use App\Discord\Core\Bot;
 use App\Discord\Core\EmbedFactory;
-use App\Models\DiscordUser;
 use Discord\Builders\MessageBuilder;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
@@ -30,49 +29,22 @@ abstract class SlashAndMessageCommand extends Command
     /**
      * I would love to move this code to the trait like I did with slash commands, but depending on where the data is going
      * (message only command, or both slash and message command) something else needs to be done.
-     * @return void
      *
-     * @TODO Fix duplicate code with block below
+     * @return void
      */
     public function registerMessageCommand(): void
     {
         Bot::get()->discord()->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-            if ($message->author->bot) {
+            if (!$this->validateMessage($message, $discord)) {
                 return;
             }
-            if (str_contains($message->content, $discord->user->id)) {
-                return;
-            }
+            $parameters = $this->processParameters($message);
+            $this->arguments = $parameters;
+            $this->messageString = join(' ', $this->arguments);
+            $this->message = $message;
+            $this->commandUser = $message->author->id;
+            $message->channel->sendMessage($this->action());
 
-            if (str_starts_with($message->content, Bot::get()->getPrefix() . $this->trigger)) {
-                if (!DiscordUser::hasLevel($message->author->id, $this->accessLevel->value)) {
-                    $message->channel->sendMessage(EmbedFactory::failedEmbed(__("bot.lack-access")));
-                    return;
-                }
-                if ($this->requiresMention && $message->mentions->count() == 0) {
-                    $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-mention')));
-                    return;
-                }
-                if ($this->requiredArguments > 0) {
-                    $parameters = explode(' ', $message->content);
-                    if (!isset($parameters[$this->requiredArguments])) {
-                        $message->channel->sendMessage(EmbedFactory::failedEmbed($this->usageString ?? __('bot.provide-arguments', ['count' => $this->requiredArguments])));
-                        return;
-                    } else {
-                        array_shift($parameters);
-                        foreach ($parameters as $index => $parameter) {
-                            if ($message->mentions->first() && str_contains($parameter, $message->mentions->first())) {
-                                $parameters[$index] = $message->mentions->first()->id;
-                            }
-                        }
-                        $this->arguments = $parameters;
-                        $this->messageString = join(' ', $this->arguments);
-                    }
-                }
-                $this->message = $message;
-                $this->commandUser = $message->author->id;
-                $message->channel->sendMessage($this->action());
-            }
         });
     }
 }
