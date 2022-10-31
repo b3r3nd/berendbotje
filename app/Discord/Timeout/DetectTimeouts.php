2fce1be2dbb5c73e544d208e657e77efb072eddc
Berend de Groot <b3r3ndd@gmail.com>
2022-10-31 17:10:54 +0100

    I really have no clue why certain data is detected as timeouts, I at least make sure the data is not corrupt from here on out and will check later what data in inserted to figure out what might cause the issue

--- app/Discord/Timeout/DetectTimeouts.php
+++ app/Discord/Timeout/DetectTimeouts.php
@@ -5,6 +5,7 @@ namespace App\Discord\Timeout;
 use App\Discord\Core\Bot;
 use Carbon\Carbon;
 use Discord\Discord;
+use Discord\Parts\Channel\Channel;
 use Discord\Parts\Guild\AuditLog\AuditLog;
 use Discord\Parts\Guild\Guild;
 use Discord\Parts\User\Member;
@@ -15,8 +16,6 @@ use Discord\WebSockets\Event;
  * reason of the timeout added to our backlog. This info is not given to us when the member who received the timeout
  * is updated. We have to manually check the last entry in the audit log in order to get the reason.
  *
- * @TODO RemoveSong hardcoded server ID
- * @TODO Read the actual right entry, it now reads the latest which - should - be OK, but there are cases where it wont work.
  */
 class DetectTimeouts
 {
@@ -26,7 +25,7 @@ class DetectTimeouts
             if ($member->communication_disabled_until == NULL) {
                 return;
             }
-            $discord->guilds->fetch('590941503917129743')->done(function (Guild $guild) use ($member) {
+            $discord->guilds->fetch($member->guild_id)->done(function (Guild $guild) use ($member) {
                 $guild->getAuditLog(['limit' => 1])->done(function (AuditLog $auditLog) use ($member) {
                     foreach ($auditLog->audit_log_entries as $entry) {
                         $endTime = $member->communication_disabled_until;
@@ -36,8 +35,8 @@ class DetectTimeouts
                             \App\Models\Timeout::create([
                                 'discord_id' => $member->id,
                                 'discord_username' => $member->username,
-                                'length' => $diff,
-                                'reason' => $entry->reason,
+                                'length' => $diff ?? 0,
+                                'reason' => $entry->reason ?? "Empty",
                             ]);
                         }
                     }
