5f87e76f03bf815c0eed3bed9606eab9274e2ba8
Berend de Groot <b3r3ndd@gmail.com>
2022-10-31 17:42:17 +0100

    Added proper pagination to timeout commands

--- app/Discord/Timeout/AllTimeouts.php
+++ app/Discord/Timeout/AllTimeouts.php
@@ -5,10 +5,12 @@ namespace App\Discord\Timeout;
 use App\Discord\Core\AccessLevels;
 use App\Discord\Core\Bot;
 use App\Discord\Core\Command\MessageCommand;
+use App\Discord\Core\Command\SlashAndMessageIndexCommand;
 use App\Discord\Core\EmbedBuilder;
 use App\Models\Timeout;
+use Discord\Parts\Embed\Embed;
 
-class AllTimeouts extends MessageCommand
+class AllTimeouts extends SlashAndMessageIndexCommand
 {
     public function accessLevel(): AccessLevels
     {
@@ -20,17 +22,18 @@ class AllTimeouts extends MessageCommand
         return 'timeouts';
     }
 
-    public function action(): void
+    public function getEmbed(): Embed
     {
+        $this->total = Timeout::count();
         $embedBuilder = EmbedBuilder::create(Bot::getDiscord())
             ->setTitle(__('bot.timeout.title'))
             ->setFooter(__('bot.timeout.footer'));
 
         $embed = $embedBuilder->getEmbed();
         $embed->setDescription(__('bot.timeout.count', ['count' => Timeout::count()]));
-        foreach (Timeout::limit(10)->orderBy('created_at', 'desc')->get() as $timeout) {
+        foreach (Timeout::skip($this->offset)->limit($this->perPage)->orderBy('created_at', 'desc')->get() as $timeout) {
             $embed = TimeoutHelper::timeoutLength($embed, $timeout);
         }
-        $this->message->channel->sendEmbed($embed);
+        return $embed;
     }
 }
--- app/Discord/Timeout/SingleUserTimeouts.php
+++ app/Discord/Timeout/SingleUserTimeouts.php
@@ -5,10 +5,13 @@ namespace App\Discord\Timeout;
 use App\Discord\Core\AccessLevels;
 use App\Discord\Core\Bot;
 use App\Discord\Core\Command\MessageCommand;
+use App\Discord\Core\Command\SlashAndMessageIndexCommand;
 use App\Discord\Core\EmbedBuilder;
 use App\Models\Timeout;
+use Discord\Parts\Embed\Embed;
+use Discord\Parts\Interactions\Command\Option;
 
-class SingleUserTimeouts extends MessageCommand
+class SingleUserTimeouts extends SlashAndMessageIndexCommand
 {
     public function accessLevel(): AccessLevels
     {
@@ -22,25 +25,36 @@ class SingleUserTimeouts extends MessageCommand
 
     public function __construct()
     {
-        parent::__construct();
         $this->requiresMention = true;
         $this->requiredArguments = 1;
         $this->usageString = __('bot.timeout.usage-timeouts');
+
+        $this->slashCommandOptions = [
+            [
+                'name' => 'user_mention',
+                'description' => 'Mention',
+                'type' => Option::USER,
+                'required' => true,
+            ],
+        ];
+
+        parent::__construct();
     }
 
-    public function action(): void
+
+    public function getEmbed(): Embed
     {
+        $this->total = Timeout::where(['discord_id' => $this->arguments[0]])->count();
         $embedBuilder = EmbedBuilder::create(Bot::getDiscord())
             ->setTitle(__('bot.timeout.title'))
             ->setFooter(__('bot.timeout.footer'));
 
         $embed = $embedBuilder->getEmbed();
-        foreach ($this->message->mentions as $mention) {
-            $embed->setDescription(__('bot.timeout.count', ['count' => Timeout::where(['discord_id' => $mention->id])->count()]));
-            foreach (Timeout::where(['discord_id' => $mention->id])->orderBy('created_at', 'desc')->get() as $timeout) {
-                $embed = TimeoutHelper::timeoutLength($embed, $timeout);
-            }
+        $embed->setDescription(__('bot.timeout.count', ['count' => $this->total]));
+        foreach (Timeout::where(['discord_id' => $this->arguments[0]])->skip($this->offset)->limit($this->perPage)->orderBy('created_at', 'desc')->get() as $timeout) {
+            $embed = TimeoutHelper::timeoutLength($embed, $timeout);
         }
-        $this->message->channel->sendEmbed($embed);
+
+        return $embed;
     }
 }
