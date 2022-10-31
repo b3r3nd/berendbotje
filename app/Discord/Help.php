d30597b9f960601eb998aa07dc969834c6dd5196
Berend de Groot <b3r3ndd@gmail.com>
2022-10-31 21:17:58 +0100

    update help file

--- app/Discord/Help.php
+++ app/Discord/Help.php
@@ -62,7 +62,7 @@ class Help extends SlashAndMessageCommand
                     );
                 } elseif ($parameters[1] === 'timeouts') {
                     $embedBuilder->getEmbed()->addField(
-                        ['name' => 'Timeouts', 'value' => 'Timeout detection and overview.'],
+                        ['name' => 'Timeouts', 'value' => 'Timeouts are automatically detected and added to this list, including the giver and reason.'],
                         ['name' => 'All timeouts', 'value' => 'timeouts'],
                         ['name' => 'Single user timeouts', 'value' => 'usertimeouts `user_mention`'],
                     );
