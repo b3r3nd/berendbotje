84590beb7d42616eae002d553f810d971172627b
Berend de Groot <b3r3ndd@gmail.com>
2022-10-31 17:30:05 +0100

    fixed timeout detection bug.

--- app/Discord/Timeout/DetectTimeouts.php
+++ app/Discord/Timeout/DetectTimeouts.php
@@ -16,15 +16,20 @@ use Discord\WebSockets\Event;
  * reason of the timeout added to our backlog. This info is not given to us when the member who received the timeout
  * is updated. We have to manually check the last entry in the audit log in order to get the reason.
  *
+ * This event triggers whenever a user is updated, when communication_disabled_until is set, and the date is not in the past
+ * it means the user got timed out. If a user is never timed out the value will be null, if a user has been timed out
+ * before it will be set to date of the previous timeout.
+ *
  */
 class DetectTimeouts
 {
     public function __construct()
     {
         Bot::getDiscord()->on(Event::GUILD_MEMBER_UPDATE, function (Member $member, Discord $discord) {
-            if ($member->communication_disabled_until == NULL) {
+            if ($member->communication_disabled_until == NULL || $member->communication_disabled_until <= Carbon::now()) {
                 return;
             }
+
             $discord->guilds->fetch($member->guild_id)->done(function (Guild $guild) use ($member) {
                 $guild->getAuditLog(['limit' => 1])->done(function (AuditLog $auditLog) use ($member) {
                     foreach ($auditLog->audit_log_entries as $entry) {
