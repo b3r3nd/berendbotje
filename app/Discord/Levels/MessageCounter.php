4c911691ea6c0f333d1e87736305e486b9bb86ae
Berend de Groot <b3r3ndd@gmail.com>
2022-11-10 00:57:34 +0100

    another fix for message counter............

--- app/Discord/Levels/MessageCounter.php
+++ app/Discord/Levels/MessageCounter.php
@@ -3,6 +3,7 @@
 namespace App\Discord\Levels;
 
 use App\Discord\Core\Bot;
+use App\Discord\Core\Guild;
 use Carbon\Carbon;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
@@ -16,12 +17,14 @@ class MessageCounter
             if ($message->author->bot) {
                 return;
             }
-            $lastMessageDate = Bot::get()->getGuild($message->guild_id)->getLastMessage($message->author->id);
             $guild = Bot::get()->getGuild($message->guild_id);
+            if ($guild) {
+                $lastMessageDate = $guild->getLastMessage($message->author->id);
 
-            if ($lastMessageDate->diffInSeconds(Carbon::now()) >= $guild->getSetting('xp_cooldown')) {
-                (new UpdateMessageCounterAction())->execute($message, $message->author->id, $guild->getSetting('xp_count'));
-                (new SyncRoleRewardsAction())->execute($message, $message->author->id);
+                if ($lastMessageDate->diffInSeconds(Carbon::now()) >= $guild->getSetting('xp_cooldown')) {
+                    (new UpdateMessageCounterAction())->execute($message, $message->author->id, $guild->getSetting('xp_count'));
+                    (new SyncRoleRewardsAction())->execute($message, $message->author->id);
+                }
             }
         });
     }
