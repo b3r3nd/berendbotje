d9801852bb982ebc9f63da5ac842444802820817
Berend de Groot <b3r3ndd@gmail.com>
2023-01-14 14:28:39 +0100

    Fixed the issue described in #13 and also made sure old pages are still us

--- app/Discord/Core/SlashIndexCommand.php
+++ app/Discord/Core/SlashIndexCommand.php
@@ -22,41 +22,24 @@ abstract class SlashIndexCommand extends SlashCommand implements PaginationIndex
     public int $perPage = 1;
     public int $total = 0;
 
-
     /**
-     * @return int
+     * @var int
+     *
+     * The user of the command is usually already available. However when using interaction commands with buttons, I
+     * need to make sure to set the user to whoever last CLICKED a button, in order to show the right data. Not the
+     * last person which used the command (which is $this->getCommandUser()). This data is available in the button
+     * listener but I cannot pass it directly through the getEmbed() function (which retrieves the actual data)
+     * since its rules are defined in the interface.
      */
-    public function getOffset(): int
-    {
-        return $this->offset[$this->getCommandUser()];
-
-    }
-
-    /**
-     * @param int $offset
-     * @return void
-     */
-    public function setOffset(int $offset): void
-    {
-        $this->offset[$this->getCommandUser()] = $offset;
-
-    }
-
-    /**
-     * @param int $amount
-     * @return void
-     */
-    public function incOffset(int $amount): void
-    {
-        $this->offset[$this->getCommandUser()] = $this->offset[$this->getCommandUser()] += $amount;
-    }
+    public int $lastUser = 0;
 
     /**
      * @return MessageBuilder
      */
     public function action(): MessageBuilder
     {
-        $this->setOffset(0);
+        $this->setLastUser($this->interaction->member->id);
+        $this->setOffset($this->interaction->member->id, 0);
         $embed = $this->getEmbed();
         $next = $this->nextButton();
         $previous = $this->previousButton()->setDisabled(true);
@@ -77,17 +60,24 @@ abstract class SlashIndexCommand extends SlashCommand implements PaginationIndex
         return Button::new(Button::STYLE_PRIMARY)
             ->setLabel(__('bot.buttons.next'))
             ->setListener(function (Interaction $interaction) {
-                $this->incOffset($this->perPage);
+                // if the person clicking on a button is not the person who received that reaction, don't do anything
+                if($interaction->member->id !== $interaction->message->interaction->user->id) {
+                    return;
+                }
+
+                $this->incOffset($interaction->member->id, $this->perPage);
                 $next = $this->nextButton();
                 $previous = $this->previousButton();
-                if (($this->getOffset() + $this->perPage) > $this->total) {
+                if (($this->getOffset($interaction->member->id) + $this->perPage) > $this->total) {
                     $next->setDisabled(true);
                 }
-                if ($this->offset > 0) {
+                if ($this->getOffset($interaction->member->id) > 0) {
                     $previous->setDisabled(false);
                 }
                 $actionRow = ActionRow::new()->addComponent($previous)->addComponent($next);
+                $this->setLastUser($interaction->member->id);
                 $interaction->message->edit(MessageBuilder::new()->addEmbed($this->getEmbed())->addComponent($actionRow));
+
 
             }, Bot::getDiscord());
     }
@@ -100,16 +90,82 @@ abstract class SlashIndexCommand extends SlashCommand implements PaginationIndex
         return Button::new(Button::STYLE_PRIMARY)
             ->setLabel(__('bot.buttons.previous'))
             ->setListener(function (Interaction $interaction) {
-                $this->incOffset($this->perPage);
+                // if the person clicking on a button is not the person who received that reaction, don't do anything
+                if($interaction->member->id !== $interaction->message->interaction->user->id) {
+                    return;
+                }
+
+                $this->decOffset($interaction->member->id, $this->perPage);
+
                 $next = $this->nextButton();
                 $previous = $this->previousButton();
-                if ($this->getOffset() === 0) {
+                if ($this->getOffset($interaction->member->id) === 0) {
                     $next->setDisabled(false);
                     $previous->setDisabled(true);
                 }
                 $actionRow = ActionRow::new()->addComponent($previous)->addComponent($next);
+                $this->setLastUser($interaction->member->id);
                 $interaction->message->edit(MessageBuilder::new()->addEmbed($this->getEmbed())->addComponent($actionRow));
 
             }, Bot::getDiscord());
+    }
+
+    /**
+     * @param int $lastUser
+     * @return void
+     */
+    public function setLastUser(int $lastUser): void
+    {
+        $this->lastUser = $lastUser;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastUser(): int
+    {
+        return $this->lastUser;
+    }
+
+
+    /**
+     * @param int $memberId
+     * @return int
+     */
+    public function getOffset(int $memberId): int
+    {
+        return $this->offset[$memberId];
+
+    }
+
+    /**
+     * @param int $memberId
+     * @param int $offset
+     * @return void
+     */
+    public function setOffset(int $memberId, int $offset): void
+    {
+        $this->offset[$memberId] = $offset;
+
+    }
+
+    /**
+     * @param int $memberId
+     * @param int $amount
+     * @return void
+     */
+    public function incOffset(int $memberId, int $amount): void
+    {
+        $this->offset[$memberId] = $this->offset[$memberId] += $amount;
+    }
+
+    /**
+     * @param int $memberId
+     * @param int $amount
+     * @return void
+     */
+    public function decOffset(int $memberId, int $amount): void
+    {
+        $this->offset[$memberId] -= $amount;
     }
 }
--- app/Discord/Levels/Leaderboard.php
+++ app/Discord/Levels/Leaderboard.php
@@ -32,15 +32,14 @@ class Leaderboard extends SlashIndexCommand
     public function getEmbed(): Embed
     {
         $this->total = \App\Models\MessageCounter::byGuild($this->guildId)->count();
-
         $description = "";
-        foreach (\App\Models\MessageCounter::byGuild($this->guildId)->orderBy('xp', 'desc')->skip($this->getOffset())->limit($this->perPage)->get() as $index => $messageCounter) {
-            $description .= Helper::indexPrefix($index, $this->getOffset());
+        foreach (\App\Models\MessageCounter::byGuild($this->guildId)->orderBy('xp', 'desc')->skip($this->getOffset($this->getLastUser()))->limit($this->perPage)->get() as $index => $messageCounter) {
+            $description .= Helper::indexPrefix($index, $this->getOffset($this->getLastUser()));
             $description .= "Level **{$messageCounter->level}** • {$messageCounter->user->tag()} • {$messageCounter->xp} xp \n";
         }
         return EmbedBuilder::create(Bot::getDiscord())
             ->setTitle(__('bot.messages.title'))
-            ->setFooter(__('bot.messages.footer', ['xp' => Bot::get()->getGuild($this->guildId)->getSetting(Setting::XP_COUNT)]))
+            ->setFooter(__('bot.messages.footer', ['xp' => Bot::get()->getGuild($this->guildId)?->getSetting(Setting::XP_COUNT)]))
             ->setDescription(__('bot.messages.description', ['users' => $description]))
             ->getEmbed();
     }
