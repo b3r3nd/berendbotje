f7ba7c21e8b053179c290bae8220221e9da96865
Berend de Groot <b3r3ndd@gmail.com>
2023-07-11 23:16:20 +0200

    Added custom level up messages

--- app/Discord/Levels/Actions/UpdateMessageCounterAction.php
+++ app/Discord/Levels/Actions/UpdateMessageCounterAction.php
@@ -70,13 +70,31 @@ class UpdateMessageCounterAction implements Action
                 $messageCounter->update(['xp' => $messageCounter->xp + $this->xpCount]);
                 $newLevel = Helper::calcLevel($messageCounter->xp);
 
+                /**
+                 * Hardcoded for now, will make proper commands to change these for each server :)
+                 */
                 if ($newLevel > $oldLevel && $guild->getSetting(Setting::ENABLE_LVL_MSG)) {
-                    $this->bot->discord->getChannel($guild->getSetting(Setting::LEVEL_UP_CHAN))?->sendMessage(__('bot.level-up-msg', ['user' => $messageCounter->user->discord_id, 'level' => $newLevel]));
+                    $msg = 'default-1';
+                    if ($newLevel > 20) {
+                        $msg = 'default-20';
+                    }
+                    if ($newLevel > 30) {
+                        $msg = 'default-30';
+                    }
+                    if ($newLevel > 40) {
+                        $msg = 'default-40';
+                    }
+                    if ($newLevel > 50) {
+                        $msg = 'default-50';
+                    }
+                    if ($newLevel === 1 || $newLevel === 5 || $newLevel === 10 || $newLevel === 20 || $newLevel === 30 || $newLevel === 40 || $newLevel === 50 || $newLevel === 60 || $newLevel === 70) {
+                        $msg = $newLevel;
+                    }
+                    $message = __("bot.lvlup.{$msg}", ['user' => $messageCounter->user->discord_id, 'level' => $newLevel]);
+                    $this->bot->discord->getChannel($guild->getSetting(Setting::LEVEL_UP_CHAN))?->sendMessage($message);
                 }
             }
         }
-
         $messageCounter->update(['level' => Helper::calcLevel($messageCounter->xp)]);
-
     }
 }
