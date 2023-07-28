74b2ed233c84e6c63c22ecc6ff390cd39190d03d
Berend de Groot <b3r3ndd@gmail.com>
2023-07-28 18:43:32 +0200

    Don't log when a message is pinned - it's in the audit log.

--- app/Discord/Logger/Events/MessageLogger.php
+++ app/Discord/Logger/Events/MessageLogger.php
@@ -37,7 +37,8 @@ class MessageLogger extends DiscordEvent
             if ($channel && !$channel->no_stickers) {
                 return;
             }
-            if (isset($oldMessage) && $guild->getLogSetting(LogSetting::MESSAGE_UPDATED) && count($oldMessage->embeds) === count($message->embeds)) {
+
+            if ($oldMessage && $oldMessage->pinned === $message->pinned && $guild->getLogSetting(LogSetting::MESSAGE_UPDATED) && count($oldMessage->embeds) === count($message->embeds)) {
                 $guild->logWithMember($message->member, __('bot.log.update-msg', ['user' => $message->member->id, 'channel' => $message->channel_id, 'old' => $oldMessage->content, 'new' => $message->content]), 'warning');
             }
         });
