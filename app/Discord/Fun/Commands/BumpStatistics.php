400d7e3f5993fbdb1d61248799c180718ec41355
Berend de Groot <b3r3ndd@gmail.com>
2023-08-01 21:38:50 +0200

    Fix bug in index om bumpstats

--- app/Discord/Fun/Commands/BumpStatistics.php
+++ app/Discord/Fun/Commands/BumpStatistics.php
@@ -52,16 +52,17 @@ class BumpStatistics extends SlashIndexCommand
     public function getEmbed(): Embed
     {
         $description = "";
-        $this->total = Bump::byGuild($this->guildId)->count();
         $builder = EmbedBuilder::create($this, __('bot.bump.title'));
 
         if (strtolower($this->getOption('date-range')) === 'all-time') {
+            $this->total = Bump::byGuild($this->guildId)->groupBy('user_id')->get()->count();
             foreach (Bump::byGuild($this->guildId)->groupBy('user_id')->orderBy('total', 'desc')->skip($this->getOffset($this->getLastUser()))->limit($this->perPage)->selectRaw('*, sum(count) as total')->get() as $index => $bumper) {
                 $description .= Helper::indexPrefix($index, $this->getOffset($this->getLastUser()));
                 $description .= "**{$bumper->user->tag()}** •  {$bumper->total}\n";
             }
             $builder->setDescription(__('bot.bump.description', ['bumpers' => $description]));
         } else {
+            $this->total = Bump::byGuild($this->guildId)->whereMonth('created_at', date('m'))->groupBy('user_id')->get()->count();
             foreach (Bump::byGuild($this->guildId)
                          ->whereMonth('created_at', date('m'))
                          ->groupBy('user_id')
