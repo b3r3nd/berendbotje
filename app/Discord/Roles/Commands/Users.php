d24bf26f7693f62f91830c19aa2807b941d4abf5
Berend de Groot <b3r3ndd@gmail.com>
2023-08-01 20:14:39 +0200

    Sort user list alphabetically

--- app/Discord/Roles/Commands/Users.php
+++ app/Discord/Roles/Commands/Users.php
@@ -38,7 +38,7 @@ class Users extends SlashIndexCommand
         $this->perPage = 20;
         $this->total = Role::byDiscordGuildId($this->guildId)->count();
         $userRoles = [];
-        foreach (Role::byDiscordGuildId(($this->guildId))->orderBy('created_at', 'asc')->skip($this->getOffset($this->getLastUser()))->limit($this->perPage)->get() as $role) {
+        foreach (Role::byDiscordGuildId(($this->guildId))->orderBy('name', 'asc')->skip($this->getOffset($this->getLastUser()))->limit($this->perPage)->get() as $role) {
             foreach ($role->users as $user) {
                 $userRoles[$user->tag()][] = $role->name;
             }
