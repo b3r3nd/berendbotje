3a2771d0af1bdf13f42809b2ee2c0d67d3270fcf
Berend de Groot <b3r3ndd@gmail.com>
2023-08-22 17:12:17 +0200

    Improve guild join/leave logging

--- app/Discord/Logger/Events/GuildMemberRemove.php
+++ app/Discord/Logger/Events/GuildMemberRemove.php
@@ -2,6 +2,7 @@
 
 namespace App\Discord\Logger\Events;
 
+use App\Discord\Core\Builders\EmbedBuilder;
 use App\Discord\Core\DiscordEvent;
 use App\Discord\Core\Interfaces\Events\GUILD_MEMBER_REMOVE;
 use App\Domain\Discord\User;
@@ -9,6 +10,7 @@ use App\Domain\Moderation\Models\BanCounter;
 use App\Domain\Moderation\Models\KickCounter;
 use App\Domain\Setting\Enums\LogSetting;
 use App\Domain\Setting\Enums\Setting;
+use Discord\Builders\MessageBuilder;
 use Discord\Discord;
 use Discord\Http\Exceptions\NoPermissionsException;
 use Discord\Parts\Guild\AuditLog\AuditLog;
@@ -34,41 +36,49 @@ class GuildMemberRemove extends DiscordEvent implements GUILD_MEMBER_REMOVE
     public function execute(Member $member, Discord $discord): void
     {
         $localGuild = $this->bot->getGuild($member->guild_id);
-        try {
-            $discord->guilds->fetch($member->guild_id)->done(function (Guild $guild) use ($member, $localGuild) {
-                $guild->getAuditLog(['limit' => 1])->done(function (AuditLog $auditLog) use ($member, $guild, $localGuild) {
-                    foreach ($auditLog->audit_log_entries as $entry) {
-                        $user = User::get($entry->user->id);
-                        if ($entry->action_type === 20 && $entry->target_id === $member->id) {
-                            if ($localGuild->getLogSetting(LogSetting::KICKED_SERVER)) {
-                                $localGuild->logWithMember($member, __('bot.log.kicked', ['user' => $member->id]), 'fail');
+        if ($member->id === $discord->user->id) {
+            $embed = EmbedBuilder::createForLog($this->bot->discord);
+            $embed->setTitle("Left guild");
+            $embed->setColor(15548997);
+            $embed->setDescription("**Name**: {$localGuild->model->name} \n **ID**: {$localGuild->model->guild_id}");
+            $this->bot->discord->getChannel(1143550163265269760)?->sendMessage(MessageBuilder::new()->addEmbed($embed));
+        } else {
+            try {
+                $discord->guilds->fetch($member->guild_id)->done(function (Guild $guild) use ($member, $localGuild) {
+                    $guild->getAuditLog(['limit' => 1])->done(function (AuditLog $auditLog) use ($member, $guild, $localGuild) {
+                        foreach ($auditLog->audit_log_entries as $entry) {
+                            $user = User::get($entry->user->id);
+                            if ($entry->action_type === 20 && $entry->target_id === $member->id) {
+                                if ($localGuild->getLogSetting(LogSetting::KICKED_SERVER)) {
+                                    $localGuild->logWithMember($member, __('bot.log.kicked', ['user' => $member->id]), 'fail');
+                                }
+                                $kickCounters = $user->kickCounters()->where('guild_id', $localGuild->model->id)->get();
+                                if ($kickCounters->isEmpty()) {
+                                    $user->kickCounters()->save(new KickCounter(['count' => 1, 'guild_id' => $localGuild->model->id]));
+                                } else {
+                                    $kickCounter = $kickCounters->first();
+                                    $kickCounter->update(['count' => $kickCounter->count + 1]);
+                                }
+                            } elseif ($entry->action_type === 22 && $entry->target_id === $member->id) {
+                                if ($localGuild->getLogSetting(LogSetting::BANNED_SERVER)) {
+                                    $localGuild->logWithMember($member, __('bot.log.banned', ['user' => $member->id]), 'fail');
+                                }
+                                $banCounters = $user->banCounters()->where('guild_id', $localGuild->model->id)->get();
+                                if ($banCounters->isEmpty()) {
+                                    $user->banCounters()->save(new BanCounter(['count' => 1, 'guild_id' => $localGuild->model->id]));
+                                } else {
+                                    $banCounter = $banCounters->first();
+                                    $banCounter->update(['count' => $banCounter->count + 1]);
+                                }
+                            } else if ($localGuild->getLogSetting(LogSetting::LEFT_SERVER)) {
+                                $localGuild->logWithMember($member, __('bot.log.left', ['user' => $member->id]), 'fail');
                             }
-                            $kickCounters = $user->kickCounters()->where('guild_id', $localGuild->model->id)->get();
-                            if ($kickCounters->isEmpty()) {
-                                $user->kickCounters()->save(new KickCounter(['count' => 1, 'guild_id' => $localGuild->model->id]));
-                            } else {
-                                $kickCounter = $kickCounters->first();
-                                $kickCounter->update(['count' => $kickCounter->count + 1]);
-                            }
-                        } elseif ($entry->action_type === 22 && $entry->target_id === $member->id) {
-                            if ($localGuild->getLogSetting(LogSetting::BANNED_SERVER)) {
-                                $localGuild->logWithMember($member, __('bot.log.banned', ['user' => $member->id]), 'fail');
-                            }
-                            $banCounters = $user->banCounters()->where('guild_id', $localGuild->model->id)->get();
-                            if ($banCounters->isEmpty()) {
-                                $user->banCounters()->save(new BanCounter(['count' => 1, 'guild_id' => $localGuild->model->id]));
-                            } else {
-                                $banCounter = $banCounters->first();
-                                $banCounter->update(['count' => $banCounter->count + 1]);
-                            }
-                        } else if ($localGuild->getLogSetting(LogSetting::LEFT_SERVER)) {
-                            $localGuild->logWithMember($member, __('bot.log.left', ['user' => $member->id]), 'fail');
                         }
-                    }
+                    });
                 });
-            });
-        } catch (NoPermissionsException) {
-            $this->bot->getGuild($member->guild_id)?->log(__('bot.exception.audit'), "fail");
+            } catch (NoPermissionsException) {
+                $this->bot->getGuild($member->guild_id)?->log(__('bot.exception.audit'), "fail");
+            }
         }
     }
 }
